d27bd83f8dfeb17cde68230ab0973addfb0cb23d
ahernandez <ahernandez@mdurance.es>
2021-04-13 23:03:30 +0200

    Integrate app roiback

---
+++ api/roiback/urls.py
@@ -0,0 +1,66 @@
+"""roiback URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path, re_path
+
+apps_patterns = [
+    path('hotels/', include('hotels.urls')),
+    path('availability/', include('inventories.urls')),
+]
+
+# General api patterns
+urlpatterns = [
+    path('api/', include(apps_patterns)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    from drf_yasg import openapi
+    from drf_yasg.views import get_schema_view
+    from rest_framework import permissions
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title=f"{settings.PROJECT_NAME} API",
+            default_version='v1',
+            description=(
+                'API for hotel availability validation'
+            ),
+            contact=openapi.Contact(
+                email="jose.alejandro.her.ros@gmail.com"
+            ),
+        ),
+        public=True,
+        permission_classes=(permissions.AllowAny,),
+    )
+
+    urlpatterns = [
+        re_path(
+            r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'
+        ),
+        path(
+            '',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'
+        ),
+        path(
+            'redoc/',
+            schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'
+        ),
+    ] + urlpatterns
